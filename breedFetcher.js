6c435730d20d4f02175925784e093e1db901acfa
Walid <walid_douri@hotmail.com>
2021-11-04 23:23:20 -0400

    First commit breedFetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,1 +1,28 @@
-//Test I lovep izza
+const request = require('request');
+
+// need to add process.arg statements/vars to remove the first two values printedd
+const breed = process.argv[2];
+
+const myCustomURL = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+
+const getData = (url) => {
+    request(url, (error, response, body) => {
+        if (error) {
+            console.log('error has occured');
+        } else {
+            const data = JSON.parse(body);
+            if (data.length > 0) {
+              console.log('Got information!');
+              console.log(data[0].description)
+            } 
+            else {
+              console.log('Could not find breed');
+            }
+        }
+    });
+}
+
+
+
+getData(myCustomURL);
+
