e47735ac78a60473e89c711fbe119fac33af17fb
Walid <walid_douri@hotmail.com>
2021-11-14 23:46:31 -0500

    Second Commit

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,28 +1,31 @@
 const request = require('request');
 
 // need to add process.arg statements/vars to remove the first two values printedd
-const breed = process.argv[2];
+//const breed = process.argv[2];
 
-const myCustomURL = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+const myCustomURL = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-const getData = (url) => {
-    request(url, (error, response, body) => {
+
+const fetchBreedDescription = function(breedName, callback) {
+    request(myCustomURL, (error, response, body) => {
         if (error) {
-            console.log('error has occured');
+            callback('Error has occured', null);
         } else {
             const data = JSON.parse(body);
+
             if (data.length > 0) {
-              console.log('Got information!');
-              console.log(data[0].description)
-            } 
-            else {
-              console.log('Could not find breed');
+                console.log('Got information!');
+                console.log(data[0].description)
+            } else {
+                callback(`Could not find breed ${breedName}`, null);
             }
         }
     });
-}
+};
 
 
+//getData(myCustomURL);
 
-getData(myCustomURL);
+module.exports = { fetchBreedDescription }
+
 
